b4f4f7690b271297cb04481795a0d844527722cd
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-05-07 14:51:51 -0300

    added pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
 import { Input } from '../components/Form/Input'
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("Formato de e-mail inválido"),
+  password: yup.string().required("Senha obrigatória")
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn:SubmitHandler<SignInFormData> = (values) => {
+    console.log(values.email)
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,17 +39,22 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input
             name="email"
             label="E-mail"
             type="email" 
+            error={formState.errors.email}
+            {...register}
           />
           <Input
             name="password"
             label="Password"
-            type="password" 
+            type="password"
+            error={formState.errors.password}
+            {...register}
           />
         </Stack>
 
